b1fe4ef8d74a4ffeb676477afbc81e07c57ea9ec
duboimax <maxencedubois22@gmail.com>
2023-01-25 08:56:29 +0100

    Ajout du model

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,15 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string nomDevise;
+        private double taux;
+
+        public int Id { get => id; set => id = value; }
+        public string NomDevise { get => nomDevise; set => nomDevise = value; }
+        public double Taux { get => taux; set => taux = value; }
+
+        public Devise() { }
+    }
+}
